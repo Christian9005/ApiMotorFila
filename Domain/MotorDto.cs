8f8ddbab1e2615b71e29c65bd8a6e92872043a39
Christian Alexander Albarracin Adriano <christianalex-071@hotmail.com>
2023-08-02 15:53:35 -0500

    updaMeter

--- Domain/MotorDto.cs
+++ Domain/MotorDto.cs
@@ -4,13 +4,13 @@ public class MotorDto
 {
     public string Name { get; set; }
     public int Speed { get; set; }
-    public int Meter { get; set; }
+    public float Meter { get; set; }
     public bool State { get; set; }
 }
 public class MotorDtoUpdate
 {
     public string Name { get; set; }
     public int? Speed { get; set; }
-    public int? Meter { get; set; }
+    public float? Meter { get; set; }
     public bool? State { get; set; }
 }
